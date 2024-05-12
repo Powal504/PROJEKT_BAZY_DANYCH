ebf4d777cefc452f85440fda30e9fb344f8f983e
Arek Kot <132007504+Arsen780@users.noreply.github.com>
2024-05-12 23:45:15 +0200

    laczenie z baza

--- zarzadzanie/src/Login/Login.jsx
+++ zarzadzanie/src/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from './Login.module.css';
 import { Link } from "react-router-dom"; 
+import axios from 'axios';
 
 function Login(){
 
@@ -15,6 +16,21 @@ function Login(){
     setPassword(event.target.value)
    }
 
+   function handleLogin(){
+    const loginData = {
+        username: username,
+        password: password
+    };
+
+    axios.post('zarz\zarz\api\Dto\LoginDto.cs', loginData)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error('Błąd logowania:', error);
+        });
+}
+
     return(
         <div className={styles.login_page}>
             <h1 className={styles.header}>ZALOGUJ SIĘ</h1>
@@ -22,7 +38,7 @@ function Login(){
             <label className={styles.label_component}>Nazwa użytkownika</label>
             <input type="password" className={styles.input_component} value={password} onChange={handlePassword}/>
             <label className={styles.label_component}>Hasło</label>
-            <button className={styles.login_button}>LOGOWANIE</button>
+            <button className={styles.login_button} onClick={handleLogin}>LOGOWANIE</button>
         </div>
     );
 }
