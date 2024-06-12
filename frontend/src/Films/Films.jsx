619837e6dd1e2c6e703922a937672edb0f0c3714
Arek Kot <132007504+Arsen780@users.noreply.github.com>
2024-06-12 05:56:48 +0200

    recenzje
    recenzje

--- frontend/src/Films/Films.jsx
+++ frontend/src/Films/Films.jsx
@@ -11,7 +11,7 @@ function Films() {
                 <img src='src/assets/Films.jpg' alt="Poster" className={styles.poster} />
                 <div className={styles.content}>
                     <p className={styles.description}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, animi inventore! Delectus autem dolorem ex, qui dolorum provident veritatis minima incidunt a corrupti sapiente ea odit exercitationem, nisi iusto. Dicta.
+                        Lorem ipsum dolor sit amcecceet consectcwwecewetur adipisicing elit. Nemo, animi inventore! Delectus autem decolorem ex, qui dolorum provident veritatis minima incidunt a corrupti sapiente ea odit exercitationem, nisi iusto. Dicta.
                     </p>
                     <div className={styles.reviews_component}>
                         <Reviews /> {/* Dodaj sekcję recenzji */}
