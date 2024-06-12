99fae48f9dcc93100d79f39b2b609bececb1acb7
Piotr Kowal <127453125+Powal504@users.noreply.github.com>
2024-06-12 02:38:11 +0200

    kk

--- frontend/src/Lists/Lists.jsx
+++ frontend/src/Lists/Lists.jsx
@@ -1,20 +1,51 @@
-import React, { useState, useContext } from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState } from "react";
 import styles from './Lists.module.css';
-import { GlobalContext } from '../GlobalContext/GlobalContext';
 
-function Lists() {
-    return(
-        <> 
-        <div className={styles.lists}>
-        <p className={styles.Add}>Stworz listy: </p>
-           
-               <input className={styles.list}></input>
-                 
-            
-           </div>
-        </>
+const Lists = () => {
+  const [catalogName, setCatalogName] = useState(""); // State to manage the catalog name
+  const [token, setToken] = useState(""); // State to manage the authentication token
 
-    );
+  // Function to handle adding items to the catalog
+  const handleAddToCatalog = async () => {
+    try {
+      const response = await fetch('http://localhost:5028/api/catalogs/CreateCatalog', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}` // Include token in the request headers
+        },
+        body: JSON.stringify({ catalog_name: catalogName })
+      });
+      
+      if (response.ok) {
+        // Handle success
+        console.log('Catalog added successfully');
+      } else {
+        // Handle errors
+        console.error('Failed to add catalog');
+      }
+    } catch (error) {
+      console.error('Error adding catalog:', error);
+    }
+  };
+
+  // Function to handle catalog name change
+  const handleCatalogNameChange = (event) => {
+    setCatalogName(event.target.value);
+  };
+
+  return (
+    <div className={styles.lists}>
+      <p className={styles.add}>Stwórz listy:</p>
+      <p>Nazwa listy</p>
+      <input
+        className={styles.listNameInput}
+        value={catalogName}
+        onChange={handleCatalogNameChange}
+      />
+      <button onClick={handleAddToCatalog} disabled={!catalogName.trim()}>Dodaj katalog</button>
+    </div>
+  );
 }
+
 export default Lists;
