b45b629aa6d679b56048f66a1ad7cdcde7697024
Arek Kot <132007504+Arsen780@users.noreply.github.com>
2024-06-13 22:52:37 +0200

    recenzje

--- zarz/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ zarz/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+163428591eea451f38565b9f8ac3a29bb7073af8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ef0efb65bfcf7ba9dd22832c778e46adad92e8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
