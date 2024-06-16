d3b003bda323364ff7db8f15b49f14cf63866c51
Piotr Kowal <127453125+Powal504@users.noreply.github.com>
2024-06-16 19:14:11 +0200

    wdq

--- zarz/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ zarz/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd0d5905a2ba58b5a066dcd6dce46b99942746e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490fa8f4c68f7eadf8bcb8ab00977826cde53a48")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
